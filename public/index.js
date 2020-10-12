706a2314f97169bf5ef707d2610c12c3486547f9
Eric <ericleroy1@gmail.com>
2020-10-12 23:24:38 +1100

    Updated graph

--- public/index.js
+++ public/index.js
@@ -1,11 +1,43 @@
 
 
-function filterFunction(){
-    const filter = document.querySelector('.checkbox:checked').value;
+function filterFunction(val){
     const trs = document.querySelectorAll('#myTable tr:not(.header)');
-    trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.includes(filter)) ? '' : 'none');
+    trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.includes(val)) ? '' : 'none');
 };
 
+function clearFilters (){
+  location.reload();
+};
+
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  while (switching) {
+
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+
+      shouldSwitch = false;
+
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+
+      if (Number(x.innerHTML) < Number(y.innerHTML)) {
+
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 // var ctx = document.getElementById('myChart2').getContext('2d');
 // var chart = new Chart(ctx, {
 //     // The type of chart we want to create
